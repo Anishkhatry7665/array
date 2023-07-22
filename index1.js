d96e9a39680710fb182e1f178d211e2a421badeb
Anish Khan <anishkhan3605@gmail.com>
2023-07-22 06:22:39 +0100

    array file

---
+++ index1.js
@@ -0,0 +1,386 @@
+// function oddEven(num1,){
+//         for(i = 1; i <= num1; i++){
+//        console.log("helloworld");
+//     }
+//      }
+//      oddEven(20)
+
+    //  function sum() {
+    //     if (30 < 20) {
+    //       return 1;
+    //     } else {
+    //       return 2;
+    //     }
+    //     return 10; 
+    //   }
+    //   let a = sum();
+    //   console.log(a); 
+    //   console.log(sum());
+
+    //   for (let i = 1; i <= 5; i++) {
+    //     let val = "";
+    //     for (let j = i; j <= 5; j++) {
+    //       val = val + "*";
+    //     }
+    //     console.log(val);
+    //   }
+    //   for (let i = 1; i <= 5; i++) {
+    //     let val = "";
+    //     for (let j = 1; j <= i; j++) {
+    //       val = val + 
+    // "*";
+    //     }
+    //     console.log(val);
+    //   }
+      
+// function sum() {
+//   if (30 < 20) {
+//     return 1;
+//   } else {
+//     return 2;
+//   }
+//   return 10; 
+// }
+// let a = sum();
+// console.log(a); 
+// console.log(sum());
+
+// function sum(){
+//     if(20<10){
+//         return 2;
+//     }
+//     else{
+//         return 3;
+//     }
+//     return 10;
+// }
+// let a=sum();
+// console.log(a);
+// console.log(sum());
+
+
+// function sum(a, b) {
+//     console.log(a + b);
+//   }
+//   let a = sum(10, 20); //Argument
+//   console.log(a);
+
+  // function sum(c,d){
+  //   console.log(c+d);
+  // }
+  // let c=sum(20,40);
+  // console.log(c);
+
+  // 7665123605
+// let str ="7665123605";
+// let end =str.substring(5);
+// console.log(end.padStart(10,"*"));
+// let start = str.substring(0,5);
+// console.log(start.padEnd(10,"*"));
+
+
+// let anish=prompt("wat is your name")
+// let ans=anish.repeat(5)
+// console.log(ans);
+
+// console.log("b".split("b"));
+// console.log("Weocde".startsWith(""));
+// console.log("Wecode".endsWith(""));
+// console.log("");
+// let str = "Sjhijvjrajaaj";
+// let arr = str.split("");
+// console.log(arr);
+// let count = 0;
+// for (let value of arr) {
+//   if (value === "j") {
+//     count++;
+//   }
+// }
+// console.log(count);
+// console.log("Academy".concat("Wecode"));
+// console.log("Academy" + "Wecode");
+// let a = "Academy";
+// let b = "Wecode";
+// console.log(`${a} ${b}`);
+
+
+// console.log("c".split("c"));
+// console.log("anish".startsWith(""));
+// console.log(anish);
+
+// var a=10;
+// var b=20;
+// if(a>b){
+//   alert("a is greater")
+// }
+// else{
+//   alert("b is greater")
+// }
+
+// confirm("do you like your name")
+
+// if(a){
+//   alert("thanx")
+// }
+// else{
+//   alert("sorry")
+// }
+
+
+// for (let i = 5; i >= 1; i--) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     if (j % 2 === 1) {
+//       str = str + "$";
+//     } else {
+//       str = str + "##";
+//     }
+//   }
+//   console.log(str);
+// }
+
+// let len = "Jhotwara, Jaipur".length;
+// if (len > 10) {
+//   for (let i = 1; i <= len; i++) {
+//     console.log(i);
+//   }
+// } else {
+//   console.log("No");
+// }
+
+// let str = "Wecode Academys"; //15
+// console.log(str.substring(0, 8));
+// console.log(str.substring(8));
+
+// let sum = "1"
+// for(let num = 5; num >=1;num--){
+//    let str  = sum.repeat(num)
+//    console.log(str);
+// }
+
+// for (let i = 5; i >= 1; i--) {
+//   console.log("$".repeat(5));
+// }
+
+// let sum ="1"
+// for(let num =5; num>= 1;num--){
+//   let str = sum.repeat(num)
+//   console.log(str);
+// }
+// for(let i = 5; i >= 1;i--){
+//   console.log("$".repeat);
+// }
+
+//  que.num.=1
+// let str = '';
+// let num = 1;
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     str += num + '';
+//     num++;
+//   }
+//   str += '\n';
+// }
+
+
+//  console.log(str);
+
+//  que.num.=2
+
+// let ans = Number(prompt("pleas enter valid your age"));
+// if (ans < 18){
+//   alert("sorry! you must be at least 18 years old to access this website, ")
+// }else if(ans <= 30 && ans >= 18){
+//   let print = confirm("Are you sure you want to proceed.?")
+//   if(print){
+//     let name = prompt("plz enter your name");
+//     alert("welcome"+name)
+//   } else{
+//     console.log("action canceled.");
+//   }
+// } else{
+//   console.log(alert("sorry! plz enter valid your age"));
+// }
+
+// 1.	Using prompt ask user below questions and then print these values in console
+// •	What is your name
+// •	What is your batch name
+// •	What is your birth year
+// Now print name, batch name and age in the console. Let's say if birth year of the user is 1991 then output should be his/her current age 31 years.
+// Ans. let Name = prompt("what is your name")
+// console.log(Name);
+// let batch = prompt("what is your batch name")
+// console.log(batch);
+// let birthday  = prompt("what is your birthday year")
+// console.log(birthday);
+
+// 2.	Print numbers like 3, 6, 9, 12, 15 till number is less than 100
+// Ans. for( let i = 3; i<=100;i++ ){   
+// console.log(i)
+// i++
+// i++}
+// 3.	Difference between truthy and falsyvalues ?
+// Ans.
+
+// Truthy value	Falsy value
+// Vah value jinka answer  hamesha
+// true aatahain	Vah value jimka answer hamesha
+// False aatahain
+// Falsy value kochodkrsbhi value truthyhotihain	Falsy value :- undefined, null,NaN, (“”),0, false yhesbfalsy value hain
+
+// 4.	Difference between implicit and explicit type conversion ?
+// Ans.
+// implicit	 explicit
+// Vah value jo automatic ek data type se dusri  data type me convert change ho jatihain	Vah value jo automatic change nhihotihain
+// Exp. 
+// let x = "10";
+// console.log(x / 5);	Exp. let a = "50";
+// console.log(Number(a)+ 100); //number to string
+// console.log(new Number(a) + 100)
+// console.log(a.toNumber()+ 100)
+// 5.	Ask user a question "Do you want to study Nodejs"? If user click on OK button then print Yes else print No in the console
+// Ans.let node = confirm("Do you want to study Nodejs" )
+//  6.	Take one number. If number is odd and less than 20 then in alert box show Yes and else show No in alert box.
+//  Ans. let x = 10
+// 10 % 2 == 0 ?alert('yes') : alert('No')
+
+//  7.	If rating is 5 then print Excellent. If rating is 4 then print Good. If rating is 3 then print Average. If rating is 2 then print Poor. If rating is 1 then print Very Poor else print Invalid rating.
+//  Ans.let i = 2
+
+// if( i == 5 ){
+// console.log("Excellent")
+// }else if(i == 4){
+// console.log("Good") 
+// }else if(i == 3){
+// console.log("Average") 
+// }else if( i === 2){
+// console.log("Poor") 
+// }else if(i == 1){
+// console.log("Very poor") 
+// }else{
+// console.log("Invalid rating") 
+// }
+//  8.	What is the significance of use strict ?
+//  Ans.Use strict :-iskamtlbhotahaikijavascript non strict mood me kuch error koignormarta h esliye un error kpodkhnekeliye use strict use me letehain
+
+//  9.	Convert any if else condition to ternary operator.
+// Ans. let x = 15
+// if(15 % 2 == 0){
+// console.log('even')
+// }else{
+// console.log('odd')
+// }
+
+//  convert to ternary operator :-
+// 15	 2 == 0 ?   console.log('even') : console.log('odd');
+
+//  10.Check if a number is divisible by 13 or not. If yes then check number is also divisible by 4 or not. If both conditions matches then print Yes else print No.
+//  Ans.
+// let x = 20 
+
+// if(x / 13 == 0 && x / 4 == 0 ){
+// console.log('yes')
+// }else{
+// console.log('No') 
+// }
+
+// ARRAY..............
+
+// 1. Array numbers ka. Loop chlana hai. Agar number odd hai to ODD print hai krna hai otherwise EVEN print krna hai
+
+// let arr = [1, 2, 3, 4, 5];
+// for (let i = 0; i < arr .length; i++){
+//   if (arr[i] % 2 === 0){
+//     console.log("even");
+//   } else {
+//     console.log("odd");
+//   }
+// }
+
+// 2. Array numbers ka. Sare numbers ko add krke print krna hai 
+
+// let arr = [1,2,3,4,5];
+// let sum = 0;
+// for (let i = 0; i < arr.length;i++){
+//   sum = sum + arr[i];
+// }
+// console.log(sum);
+
+// 3. Array numbers ka. Sare Odd numbers ko add krke print krna hai 
+
+// let arr = [1,2,3,4,5,6,7];
+// let sum = 0;
+// for(let i = 0;i < arr.length;i++){
+//   if (arr[i] % 2 === 1){
+//     sum = sum + arr[i];
+//   }
+// }
+// console.log(sum);
+
+// 4. Array numbers ka. Sare Even numbers ko add krke print krna hai 
+
+// let arr =  [1,2,3,4,5,6,7]
+// let sum = 0;
+// for(let i = 0; i <arr.length; i++){
+//   if (arr[i] % 2 === 0){
+//     sum = sum + arr[i];
+//   }
+// }
+// console.log(sum);
+
+// 5. Array numbers ka. Maximum number btana hai 
+
+// let arr =  [1,2,53,47,7,8]
+// let max = arr[0];
+// for (let i = 0; i < arr.length; i++){
+//   if (arr[i] > max){
+//     max = arr[i]
+//   }
+// }
+// console.log(max);
+
+// 6. Array numbers ka. Minimum number btana ha
+
+// let arr = [2,3,4,1,5]
+// let min = arr[0];
+// for (let i = 0; i <arr.length; i++){
+//   if (arr[i] < min){
+//     min = arr[i]
+//   }
+// }
+// console.log(min);
+
+// 7. Array ka phla number print krvana hai 
+
+// let arr = [1, 2, 30, 0, -1, 4, 5];
+// console.log(arr[0]);
+// console.log(arr[arr.length - 1]);
+
+// 8. Array ka last number print krvana hai 
+
+// let arr = [1, 2, 30, 0, -1, 4, 5];
+// console.log(arr[0]);
+// console.log(arr[arr.length - 1]);
+
+
+// 9. Array me kitne numbers aise hai jo 100 se bde hai 
+
+// let arr = [1,300,100,40,200,250,500,]
+// let count = 0;
+// for (let i = 0; i < arr.length; i++){
+//   if (arr[i] > 100){
+//     ++count;
+//   }
+// }
+// console.log(count);
+
+// 10. Array me kitne numbers aise hai jo 20 se bde or Even hai 
+
+// let arr = [2,30,40,250,10,44];
+// let concat = 0;
+// for (let i = 0; i < arr.length; i++){
+//   if (arr[i] > 20 && arr[i] % 2 === 0){
+//     ++concat;
+//   }
+// }
+// console.log(concat);
